c4494ee376f6abd9baeb037e9b6ec7d5bbb0f2b1
Daniel Ferreira <daniel.ferreira@cyansecurity.com>
2022-09-20 15:37:56 +0200

    add determinism tests

--- tests/test_nonce2vec.py
+++ tests/test_nonce2vec.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+import numpy as np
+
 from nonce2vec.models.informativeness import Informativeness
 from nonce2vec.models.nonce2vec import Nonce2Vec
 
@@ -53,3 +56,35 @@ def test_add_nonces_correctness(w2v_models, test_sentences):
     n2v_model.add_nonces(test_sentences)
     assert n2v_model.model.wv.similarity("giraffes", "elephants") > 0.99
     assert n2v_model.model.wv.similarity("giraffes", "yet") < 0.5
+
+
+def test_determinism(w2v_models, test_sentences):
+    sg_model, cbow_model = w2v_models
+    info_model_obj = Informativeness(cbow_model)
+    n2v_model1 = Nonce2Vec(deepcopy(sg_model), info_model_obj, reduced=True)
+    n2v_model1.add_nonces(test_sentences)
+    n2v_model2 = Nonce2Vec(deepcopy(sg_model), info_model_obj, reduced=True)
+    n2v_model2.add_nonces(test_sentences)
+
+    np.testing.assert_array_equal(n2v_model1.model.wv.get_vector("cats"),
+                                  n2v_model2.model.wv.get_vector("cats"))
+    np.testing.assert_array_equal(n2v_model1.model.wv.get_vector("giraffes"),
+                                  n2v_model2.model.wv.get_vector("giraffes"))
+
+
+def test_add_nonces_multiple_occurences(w2v_models, test_sentences):
+    sg_model, cbow_model = w2v_models
+    info_model_obj = Informativeness(cbow_model)
+    n2v_model1 = Nonce2Vec(deepcopy(sg_model), info_model_obj)
+    n2v_model2 = Nonce2Vec(deepcopy(sg_model), info_model_obj)
+
+    n2v_model1.add_nonces([test_sentences[0]])
+    n2v_model2.add_nonces(test_sentences)
+
+    # these vectors should be the same
+    np.testing.assert_array_equal(n2v_model1.model.wv.get_vector("cats"),
+                                  n2v_model2.model.wv.get_vector("cats"))
+    with np.testing.assert_raises(AssertionError):
+        # this should raise an error, the vectors should be different
+        np.testing.assert_array_equal(n2v_model1.model.wv.get_vector("giraffes"),
+                                      n2v_model2.model.wv.get_vector("giraffes"))
